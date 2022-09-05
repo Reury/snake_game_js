34599d02a005a22d0f366a4a53fc502fcf3a3086
Reury <reuryaraujo@gmail.com>
2022-09-05 02:36:13 -0300

    fim do jogo

--- script.js
+++ script.js
@@ -43,10 +43,23 @@ function update(event){
 }
 
 function iniciarJogo(){
+
+    for(i=1;i<snake.length;i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Voce perdeu :(");
+            
+        }
+
+    }
+
+
     if(snake[0].x  > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x  < 0 * box && direction == "left") snake[0].x = 16*box;
     if(snake[0].y  > 15 * box && direction == "up") snake[0].y = 0;
     if(snake[0].y  < 0   && direction == "down") snake[0].y = 16*box;
+
+     
     
 
     criarBG();
@@ -61,7 +74,19 @@ function iniciarJogo(){
     if(direction == "up") snakeY += box;
     if(direction == "down") snakeY -= box;
 
-    snake.pop();
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+
+
+    }
+
+
 
     let newHead = {
         x: snakeX,
